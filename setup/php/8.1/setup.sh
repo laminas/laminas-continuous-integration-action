77810a324d486ccdd8367b80d5f5449289bc6077
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-08-17 23:48:44 +0200

    bugfix: handle latest changes made by `shivammathur/php-builder`
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- setup/php/8.1/setup.sh
+++ setup/php/8.1/setup.sh
@@ -4,25 +4,16 @@ curl -sSLO https://github.com/shivammathur/php-builder/releases/latest/download/
 chmod a+x ./install.sh
 ./install.sh 8.1
 
-# Setup directory structure
-CONFIGURATION_DIRECTORY_PREFIX="/usr/local/php/8.1/etc"
-
-rm -rf /etc/php/8.1
-mkdir -p /etc/php/8.1/{cli,mods-available}
-ln -s $CONFIGURATION_DIRECTORY_PREFIX/php.ini /etc/php/8.1/cli/
-ln -s $CONFIGURATION_DIRECTORY_PREFIX/conf.d /etc/php/8.1/cli/conf.d
+# Keep modules we do have in PHP 8.0 as well while disabling all other modules
 for mod in /etc/php/8.1/cli/conf.d/*; do
     modWithoutPriority=$(echo $(basename $mod) | cut -d'-' -f2-)
     modname=$(echo $modWithoutPriority | cut -d'.' -f1)
 
-    mv $mod /etc/php/8.1/mods-available/$modWithoutPriority
-
     case "$modname" in
-        "xdebug" | "sqlsrv" | "pdo_sqlsrv")
-            # Do not enable these modules as default
+        "bz2" | "calendar" | "ctype" | "curl" | "dom" | "exif" | "ffi" | "fileinfo" | "ftp" | "gettext" | "iconv" | "intl" | "mbstring" | "opcache" | "pdo" | "phar" | "posix" | "readline" | "shmop" | "simplexml" | "sockets" | "sysvmsg" | "sysvsem" | "sysvshm" | "tokenizer" | "xml" | "xmlreader" | "xmlwriter" | "xsl" | "zip")
         ;;
         *)
-            ln -s /etc/php/8.1/mods-available/$modWithoutPriority $mod
+            rm $mod
         ;;
     esac
 done
