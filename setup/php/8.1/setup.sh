a097c3c7b4649340af19a69151a32991509ef8b9
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-08-17 23:57:27 +0200

    qa: apply latest shellcheck hints
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- setup/php/8.1/setup.sh
+++ setup/php/8.1/setup.sh
@@ -5,15 +5,14 @@ chmod a+x ./install.sh
 ./install.sh 8.1
 
 # Keep modules we do have in PHP 8.0 as well while disabling all other modules
-for mod in /etc/php/8.1/cli/conf.d/*; do
-    modWithoutPriority=$(echo $(basename $mod) | cut -d'-' -f2-)
-    modname=$(echo $modWithoutPriority | cut -d'.' -f1)
+for moduleIniSettings in /etc/php/8.1/cli/conf.d/*; do
+    moduleName=$(echo "$(basename $(readlink $moduleIniSettings))" | cut -d '.' -f1)
 
-    case "$modname" in
+    case "$moduleName" in
         "bz2" | "calendar" | "ctype" | "curl" | "dom" | "exif" | "ffi" | "fileinfo" | "ftp" | "gettext" | "iconv" | "intl" | "mbstring" | "opcache" | "pdo" | "phar" | "posix" | "readline" | "shmop" | "simplexml" | "sockets" | "sysvmsg" | "sysvsem" | "sysvshm" | "tokenizer" | "xml" | "xmlreader" | "xmlwriter" | "xsl" | "zip")
         ;;
         *)
-            rm $mod
+            rm "$moduleIniSettings"
         ;;
     esac
 done
