f1841f01fcf43c31e2917429b9575a3ba5d37b4a
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-08-19 18:59:41 +0200

    bugfix: do not disable any modules
    Modules may depend on each other and thus, if one is enabled (e.g. `redis`), it will also require `msgpack`, `igbinary` and stuff to be enabled on PHP 8.1
    
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- setup/php/8.1/setup.sh
+++ setup/php/8.1/setup.sh
@@ -4,20 +4,6 @@ curl -sSLO https://github.com/shivammathur/php-builder/releases/latest/download/
 chmod a+x ./install.sh
 ./install.sh 8.1
 
-# Keep modules we do have in PHP 8.0 as well while disabling all other modules
-for moduleIniSettings in /etc/php/8.1/cli/conf.d/*; do
-    # shellcheck disable=SC2005
-    moduleName=$(echo "$(basename "$(readlink "$moduleIniSettings")")" | cut -d '.' -f1)
-
-    case "$moduleName" in
-        "bz2" | "calendar" | "ctype" | "curl" | "dom" | "exif" | "ffi" | "fileinfo" | "ftp" | "gettext" | "iconv" | "intl" | "mbstring" | "opcache" | "pdo" | "phar" | "posix" | "readline" | "shmop" | "simplexml" | "sockets" | "sysvmsg" | "sysvsem" | "sysvshm" | "tokenizer" | "xml" | "xmlreader" | "xmlwriter" | "xsl" | "zip")
-        ;;
-        *)
-            rm "$moduleIniSettings"
-        ;;
-    esac
-done
-
 # Prepares the configuration the same way as mods-install/install_sqlsrv.sh would do
 cat /etc/php/8.1/mods-available/pdo_sqlsrv.ini >> /etc/php/8.1/mods-available/sqlsrv.ini
 rm /etc/php/8.1/mods-available/pdo_sqlsrv.ini
