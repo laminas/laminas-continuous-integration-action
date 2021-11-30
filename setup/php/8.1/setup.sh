c11b6c6f38387a94dbeda1051e6779f6033e3ce8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-11-30 14:59:51 -0600

    feat: switch back to ondrej sury repo for 8.1
    With a stable 8.1, we can revert back to the sury repo to ensure consistency in the PHP build versions and ability to enable/disable extensions.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- setup/php/8.1/setup.sh
+++ setup/php/8.1/setup.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-curl -sSLO https://github.com/shivammathur/php-builder/releases/latest/download/install.sh
-chmod a+x ./install.sh
-./install.sh 8.1
-
-# Prepares the configuration the same way as mods-install/install_sqlsrv.sh would do
-cat /etc/php/8.1/mods-available/pdo_sqlsrv.ini >> /etc/php/8.1/mods-available/sqlsrv.ini
-rm /etc/php/8.1/mods-available/pdo_sqlsrv.ini
+ACCEPT_EULA=Y xargs -a dependencies apt install -y
