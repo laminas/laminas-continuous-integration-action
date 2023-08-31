d6057a8da99bb7f81472f7be45d8af2a910a0a46
Maurício Meneghini Fauth <mauricio@fauth.dev>
2023-08-30 21:56:19 -0300

    Update sqlsrv extension to 5.11 and add PHP 8.2 support
    Signed-off-by: Maurício Meneghini Fauth <mauricio@fauth.dev>

--- mods-install/install_sqlsrv.sh
+++ mods-install/install_sqlsrv.sh
@@ -5,16 +5,19 @@ set -e
 cd tmp
 
 # Download extension versions from MS
-curl -L https://github.com/microsoft/msphpsql/releases/download/v5.10.0/Ubuntu2004-8.0.tar | tar xf - --strip-components=1 Ubuntu2004-8.0/php_pdo_sqlsrv_80_nts.so Ubuntu2004-8.0/php_sqlsrv_80_nts.so
-curl -L https://github.com/microsoft/msphpsql/releases/download/v5.10.0/Ubuntu2004-8.1.tar | tar xf - --strip-components=1 Ubuntu2004-8.1/php_pdo_sqlsrv_81_nts.so Ubuntu2004-8.1/php_sqlsrv_81_nts.so
+curl -L https://github.com/microsoft/msphpsql/releases/download/v5.11.0/Ubuntu2004-8.0.tar | tar xf - --strip-components=1 Ubuntu2004-8.0/php_pdo_sqlsrv_80_nts.so Ubuntu2004-8.0/php_sqlsrv_80_nts.so
+curl -L https://github.com/microsoft/msphpsql/releases/download/v5.11.0/Ubuntu2004-8.1.tar | tar xf - --strip-components=1 Ubuntu2004-8.1/php_pdo_sqlsrv_81_nts.so Ubuntu2004-8.1/php_sqlsrv_81_nts.so
+curl -L https://github.com/microsoft/msphpsql/releases/download/v5.11.0/Ubuntu2004-8.2.tar | tar xf - --strip-components=1 Ubuntu2004-8.2/php_pdo_sqlsrv_82_nts.so Ubuntu2004-8.2/php_sqlsrv_82_nts.so
 
 # Copy extensions to appropriate locations for each PHP version
 mv php_pdo_sqlsrv_80_nts.so "$(php8.0 -i | grep -P '^extension_dir' | sed -E -e 's/^extension_dir\s+=>\s+\S+\s+=>\s+(.*)$/\1/')/pdo_sqlsrv.so"
 mv php_sqlsrv_80_nts.so "$(php8.0 -i | grep -P '^extension_dir' | sed -E -e 's/^extension_dir\s+=>\s+\S+\s+=>\s+(.*)$/\1/')/sqlsrv.so"
 mv php_pdo_sqlsrv_81_nts.so "$(php8.1 -i | grep -P '^extension_dir' | sed -E -e 's/^extension_dir\s+=>\s+\S+\s+=>\s+(.*)$/\1/')/pdo_sqlsrv.so"
 mv php_sqlsrv_81_nts.so "$(php8.1 -i | grep -P '^extension_dir' | sed -E -e 's/^extension_dir\s+=>\s+\S+\s+=>\s+(.*)$/\1/')/sqlsrv.so"
+mv php_pdo_sqlsrv_82_nts.so "$(php8.2 -i | grep -P '^extension_dir' | sed -E -e 's/^extension_dir\s+=>\s+\S+\s+=>\s+(.*)$/\1/')/pdo_sqlsrv.so"
+mv php_sqlsrv_82_nts.so "$(php8.2 -i | grep -P '^extension_dir' | sed -E -e 's/^extension_dir\s+=>\s+\S+\s+=>\s+(.*)$/\1/')/sqlsrv.so"
 
 # Copy conf file to appropriate locations
-for PHP_VERSION in 8.0 8.1;do
+for PHP_VERSION in 8.0 8.1 8.2;do
     cp /mods-available/sqlsrv.ini "/etc/php/${PHP_VERSION}/mods-available/sqlsrv.ini"
 done
