42e4c4afc0f8d2cbf0847a0011e813c6c7acaeef
Aleksei Khudiakov <aleksey@xerkus.pro>
2024-05-10 02:47:26 +1000

    Specify files to extract from sqlsrv release asset
    Signed-off-by: Aleksei Khudiakov <aleksey@xerkus.pro>

--- mods-install/install_sqlsrv.sh
+++ mods-install/install_sqlsrv.sh
@@ -8,7 +8,10 @@ cd tmp
 curl -L https://github.com/microsoft/msphpsql/releases/download/v5.9.0/Ubuntu2004-7.3.tar | tar xf - --strip-components=1 Ubuntu2004-7.3/php_pdo_sqlsrv_73_nts.so Ubuntu2004-7.3/php_sqlsrv_73_nts.so
 curl -L https://github.com/microsoft/msphpsql/releases/download/v5.10.1/Ubuntu2004-7.4.tar | tar xf - --strip-components=1 Ubuntu2004-7.4/php_pdo_sqlsrv_74_nts.so Ubuntu2004-7.4/php_sqlsrv_74_nts.so
 curl -L https://github.com/microsoft/msphpsql/releases/download/v5.11.1/Ubuntu2204-8.0.tar | tar xf - --strip-components=1 Ubuntu2204-8.0/php_pdo_sqlsrv_80_nts.so Ubuntu2204-8.0/php_sqlsrv_80_nts.so
-curl -L https://github.com/microsoft/msphpsql/releases/download/v5.12.0/Linux_5.12.0RTW.tar.gz | tar xzf - --strip-components=2
+curl -L https://github.com/microsoft/msphpsql/releases/download/v5.12.0/Linux_5.12.0RTW.tar.gz | tar xzf - --strip-components=2 \
+    ./Linux/PDO_SQLSRV_8.1_NTS.so ./Linux/SQLSRV_8.1_NTS.so \
+    ./Linux/PDO_SQLSRV_8.2_NTS.so ./Linux/SQLSRV_8.2_NTS.so \
+    ./Linux/PDO_SQLSRV_8.3_NTS.so ./Linux/SQLSRV_8.3_NTS.so
 
 # Copy extensions to appropriate locations for each PHP version
 mv php_pdo_sqlsrv_73_nts.so "$(php7.3 -i | grep -P '^extension_dir' | sed -E -e 's/^extension_dir\s+=>\s+\S+\s+=>\s+(.*)$/\1/')/pdo_sqlsrv.so"
